8859b3e951c6117608b06550883afcbdc6938770
gitshamilc <shamilcv61@gmail.com>
2026-06-03 11:24:23 +0530

    Delete supabase_client.js

--- supabase_client.js
+++
@@ -1,6 +0,0 @@
-// Initialize Supabase Client
-const { createClient } = window.supabase || {};
-
-export const supabase = createClient
-  ? createClient(window.CONFIG.SUPABASE_URL, window.CONFIG.SUPABASE_ANON_KEY)
-  : null;
